14a295c5eca83a20b5eb4fbe3f8ed332a26f1724
Chandler DeLoach <deloach.cdd@gmail.com>
2022-06-16 21:59:06 -0600

    oh man, I messed up, 'core' isn't a module!

--- script/create_provisioner.sh
+++ script/create_provisioner.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -euo pipefail
+
 indented_cat() {
     awk -v N="$2" '{ for(i=0;i<N;i++) printf " "; print $0 }' "$1" 
 }
@@ -6,10 +8,11 @@ indented_cat() {
 if [[ ! -d ansible ]]; then
     mkdir ansible
 else
+    # TODO warn they're gonna nuke the existing provisioner!
     rm -r ansible/*
 fi
 
-for file in setup/inventory.ini $(find modules | grep inventory.ini | xargs); do
+for file in core/setup/inventory.ini $(find modules | grep inventory.ini | xargs); do
     cat $file
     echo
 done > ansible/inventory.ini
@@ -19,30 +22,52 @@ cat ansible/inventory.ini
 cat > ansible/provision.yml << EOF
 hosts: gb_host
 roles:
-  - setup
   - docker
-  - nginx
+  - setup
+  - proxy
 $(grep -v '^\s*#' services.yml | tail -n +2)
   - finalize
 EOF
 
 cat ansible/provision.yml
 
-create_ansible_role() {
+create_basic_ansible_role() {
     ROLE=$1
     # Generate a bunch of symlinks to create fleshed out ansible roles for our modules
     ROLE_HOME=ansible/$(basename $ROLE)
+    # Main logic
     mkdir -p $ROLE_HOME/tasks
     ln -s $ROLE/tasks.yml $ROLE_HOME/tasks/main.yml
 }
 
+create_full_ansible_role() {
+    ROLE=$1
+    create_basic_ansible_role $ROLE
+    # Files the main logic needs access to
+    if [[ -d $ROLE/files ]]; then
+        ln -s $ROLE/files $ROLE_HOME/files
+    fi
+    # Templates the main logic needs access to
+    if [[ -d $ROLE/templates ]]; then
+        ln -s $ROLE/templates $ROLE_HOME/templates
+    fi
+}
+
+# TODO create roles from modules
+
+create_basic_ansible_role core/finalize
+mkdir ansible/finalize/templates
 # Construct templated docker-compose.yml file for all our containerized services
-cat > docker-compose.yml.j2 << EOF
+DOCKER_COMPOSE_FILE=ansible/finalize/templates/docker-compose.yml.j2
+cat > $DOCKER_COMPOSE_FILE << EOF
 version: "3"
 services:
 EOF
 for file in $(find modules | grep docker-compose.yml | xargs); do
     indented_cat $file 2
     echo
-done >> docker-compose.yml.j2
-cat docker-compose.yml.j2
+done >> $DOCKER_COMPOSE_FILE
+cat $DOCKER_COMPOSE_FILE
+
+# TODO firewall
+# TODO nginx reverse proxy
