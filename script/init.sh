f578445c1fc92dd146ff3aba64a96ce9a96507cf
Chandler DeLoach <deloach.cdd@gmail.com>
2023-02-22 14:26:45 -0700

    symlink to docker-compose.yml.j2 for convenience/consistency

--- script/init.sh
+++ script/init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 create_inventory_file_from_template() {
-    echo "Creating 'user/inventory.yml' from template."
+    echo "Creating 'user/inventory.yml' from template..."
     cp templates/inventory.yml user/inventory.yml
 }
 
@@ -18,7 +18,7 @@ if [[ ! -d user ]]; then
 fi
 
 if [[ ! -e user/secrets.yml ]]; then
-    echo "Creating 'user/secrets.yml' from template."
+    echo "Creating 'user/secrets.yml' from template..."
     cp templates/secrets.yml user/secrets.yml
     chmod 600 user/secrets.yml
 fi
@@ -33,5 +33,10 @@ if [[ -e user/inventory.yml ]]; then
 else
     create_inventory_file_from_template
 fi
+
+echo "Creating docker-compose.yml.j2 from template..."
+cp templates/docker-compose.yml.j2 roles/finalize/templates/docker-compose.yml.j2
+ln -s roles/finalize/templates/docker-compose.yml.j2 user/docker-compose.yml.j2
+
 echo
 echo "Initialized succesfully!"
